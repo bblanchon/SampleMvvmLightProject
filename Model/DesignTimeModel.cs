b89176ff0372431b7ccd3d51d35ba1145c189ae2
Benoit Blanchon <benoit.blanchon@gmail.com>
2013-10-17 22:22:00 +0200

    Added a property in the ViewModel and show the value in the View. Added a run time and a design time model.

---
+++ Model/DesignTimeModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleMvvmLightProject.Model
+{
+    class DesignTimeModel : IModel
+    {
+        public string SomeText
+        {
+            get { return "I'm an instance of DesignTimeModel"; }
+        }
+    }
+}
--- ViewModel/MainViewModel.cs
+++ ViewModel/MainViewModel.cs
@@ -1,34 +1,39 @@
 using GalaSoft.MvvmLight;
+using SampleMvvmLightProject.Model;
 
 namespace SampleMvvmLightProject.ViewModel
 {
-    /// <summary>
-    /// This class contains properties that the main View can data bind to.
-    /// <para>
-    /// Use the <strong>mvvminpc</strong> snippet to add bindable properties to this ViewModel.
-    /// </para>
-    /// <para>
-    /// You can also use Blend to data bind with the tool's support.
-    /// </para>
-    /// <para>
-    /// See http://www.galasoft.ch/mvvm
-    /// </para>
-    /// </summary>
     public class MainViewModel : ViewModelBase
     {
+        readonly IModel model;
+        string someText;
+
         /// <summary>
         /// Initializes a new instance of the MainViewModel class.
         /// </summary>
         public MainViewModel()
         {
-            ////if (IsInDesignMode)
-            ////{
-            ////    // Code runs in Blend --> create design time data.
-            ////}
-            ////else
-            ////{
-            ////    // Code runs "for real"
-            ////}
+            if (IsInDesignMode)
+            {
+                // code runs in blend --> create design time data.
+                model = new DesignTimeModel();
+            }
+            else
+            {
+                // code runs "for real"
+                model = new RunTimeModel();
+            }
+
+            SomeText = model.SomeText;
+        }
+
+        public string SomeText
+        {
+            get { return someText; }
+            set 
+            {
+                Set(() => SomeText, ref someText, value);
+            }
         }
     }
 }
